f8217d7653ed77edba784d9cc93b6ba7ccf98c5d
Rizki <rizkikurniawan1797@gmail.com>
2020-12-11 23:14:17 +0700

    Updated field

--- app/src/main/java/com/kikulabs/noteusingroom/entity/Note.kt
+++ app/src/main/java/com/kikulabs/noteusingroom/entity/Note.kt
@@ -15,5 +15,6 @@ data class Note(
     //ColumnInfo annotation to specify the column's name
     @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "id") var id: Int = 0,
     @ColumnInfo(name = "title") var title: String = "",
+    @ColumnInfo(name = "label") var label: String = "",
     @ColumnInfo(name = "body") var body: String = ""
 ) : Parcelable

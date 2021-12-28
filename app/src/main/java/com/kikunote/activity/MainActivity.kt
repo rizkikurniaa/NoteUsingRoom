f342c74d7cb8bf4226229965d0369faf3d108477
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-12-28 21:42:23 +0700

    Implemented in app update

--- app/src/main/java/com/kikunote/activity/MainActivity.kt
+++ app/src/main/java/com/kikunote/activity/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.kikunote.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.kikunote.R
 import com.kikunote.adapter.SectionsPagerAdapter
 import com.kikunote.databinding.ActivityMainBinding
@@ -11,6 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         initView()
         initListener()
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        appUpdateInfoTask.addOnSuccessListener {
+            if (it.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && it.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    it,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    999
+                )
+            } else {
+                // TODO: do something in here if update not available
+            }
+        }
 
     }
 
@@ -48,6 +71,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.floatingActionButton -> {
                 startActivity(Intent(this, EditActivity::class.java))
             }
+        }
+    }
+
+    override fun onResume() {
+        appUpdateManager.appUpdateInfo
+            .addOnSuccessListener {
+                if (it.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        it,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        999
+                    )
+                }
+            }
+        super.onResume()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 999 && resultCode == Activity.RESULT_OK) {
+            // TODO: do something in here if in-app updates success
+        } else {
+            // TODO: do something in here if in-app updates failure
         }
     }
 }
